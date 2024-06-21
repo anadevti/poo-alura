dc60a2b84e70f994ab478eaffc47f8ca4a1ff912
anadevti <anacarolayne777@gmail.com>
2024-06-21 11:28:05 -0300

    subindo novas funcoes

--- curso-python-2-aula_5/app.py
+++ curso-python-2-aula_5/app.py
@@ -5,10 +5,10 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de melancia', 5.0, '300ml')
 prato_paozinho = Prato('Pão de queijo', 2.0, '100g')
-
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
--- curso-python-2-aula_5/modelos/restaurante.py
+++ curso-python-2-aula_5/modelos/restaurante.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-
+from modelos.cardapio.item_cardapio import ItemCardapio
 class Restaurante:
     restaurantes = []
 
@@ -8,6 +8,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -39,3 +40,13 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
+        
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+    
+    @property        
+    def exibir_cardapio(self):
+        print(f'Cardápio do Restaurante {self._nome}\n')
+        for item in self._cardapio:
+            print(item)
